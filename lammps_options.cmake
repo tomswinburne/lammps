e0d6b0981e930d67ccee440838505a465e5fcc01
tomswinburne <tomswinburne@gmail.com>
2023-05-23 10:54:17 -0700

    small update

--- lammps_options.cmake
+++ lammps_options.cmake
@@ -3,6 +3,9 @@
 # set installation location
 set(CMAKE_INSTALL_PREFIX "$ENV{PREFIX}" CACHE BOOL "" FORCE)
 
+#set(Python_FIND_FRAMEWORK LAST)
+#find_package(Python 3.11 EXACT REQUIRED)
+
 # enforce c++11 standards
 set(CCFLAGS -g -O3 -std=c++11)
 
@@ -13,7 +16,7 @@ set(BUILD_SHARED_LIBS ON CACHE BOOL "" FORCE)
 set(LAMMPS_EXCEPTIONS ON CACHE BOOL "" FORCE)
 
 # minimal packages to run example (MANYBODY, EXTRA-FIX) and generate new pathway (REPLICA for "fix neb")
-set(ALL_PACKAGES MANYBODY EXTRA-FIX REPLICA MOLECULE ML-SNAP)
+set(ALL_PACKAGES MANYBODY EXTRA-FIX REPLICA ML-SNAP ML-PACE PYTHON PHONON)
 
 foreach(PKG ${ALL_PACKAGES})
   set(PKG_${PKG} ON CACHE BOOL "" FORCE)
